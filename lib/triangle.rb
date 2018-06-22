bccde798b63fd092c1e006133300d9466a2d4fb3
David Hopkins <dlhopkins2@gmail.com>
2018-06-22 20:40:25 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,30 +1,37 @@
 class Triangle
-  attr_accessor :length, :width, :height
+  attr_accessor :a, :b, :c
 
-  def initialize(length, width, height)
-    @length = length
-    @width = width
-    @height = height
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    if @length <= 0 || @width <= 0 || @height <= 0
+    #valid?
+    if a <= 0 || b <= 0 || c <= 0
       raise TriangleError
     end
 
-    if @length + @width <= @height || @width + @height <= @length || @length + @height <= @width
+    if a + b <= c || b + c <= a || a + c <= b
       raise TriangleError
-      end
+    end
 
-    if @length == @width && @width == @height
+    if a == b && b == c
       :equilateral
     elsif
-      @length == @width || @width == @height || @length == @height
+      a == b || b == c || a == c
       :isosceles
     else
       :scalene
     end
   end
+
+#   def valid?
+#     real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+#     [a, b, c].each { |s| real_triangle << false if s <= 0 }
+#       raise TriangleError if real_triangle.include?(false)
+#   end
 
   class TriangleError < StandardError
   end
