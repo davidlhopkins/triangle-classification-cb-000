9142481d17251348183744595728b88b3637fea8
David Hopkins <dlhopkins2@gmail.com>
2018-06-22 20:29:04 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :length, :width, :height
+
+  def initialize(length, width, height)
+    @length = length
+    @width = width
+    @height = height
+  end
+
+  def kind
+    if @length <= 0 || @width <= 0 || @height <= 0
+      raise TriangleError
+    end
+
+    if @length + @width <= @height || @width + @height <= @length || @length + @height <= @width
+      raise TriangleError
+      end
+
+    if @length == @width && @width == @height
+      :equilateral
+    elsif
+      @length == @width || @width == @height || @length == @height
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
